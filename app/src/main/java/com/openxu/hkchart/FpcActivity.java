f25bf3f93728e4376a626838ad4cd5b0386c3718
openXu <openXu@yeah.net>
2022-02-12 11:47:26 +0800

    a

--- app/src/main/java/com/openxu/hkchart/FpcActivity.java
+++ app/src/main/java/com/openxu/hkchart/FpcActivity.java
@@ -5,6 +5,8 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
